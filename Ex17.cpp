fd291e3bb2ab3a19cba85d0a751aa68c3d84a4c1
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-26 22:58:32 -0300

    Create Ex17.cpp

---
+++ Ex17.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+ int main() {
+    float n1,n2,n3,MP;
+    cout<<"Digite, respectivamente, o valor de suas notas de n1, n2 e n3";
+    cin>>n1>>n2>>n3;
+    MP=(n1*2+n2*1+n3*3)/6;
+    cout<<"Sua média final é: "<
+    return 0;
+}
