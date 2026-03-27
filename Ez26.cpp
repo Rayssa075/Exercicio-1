fc8c9824017f98213df59c271d9f03b0b9cb4a6a
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-26 23:00:44 -0300

    Create Ez26.cpp

---
+++ Ez26.cpp
@@ -0,0 +1,10 @@
+#include  <iostream>
+
+using namespace std;
+int main () { 
+    float p, PE;
+    cout<<"insira seu peso";
+    cin>>p;
+    PE=p*1.5;
+    cout<<"seu peso engordando 15% é: "<
+}
