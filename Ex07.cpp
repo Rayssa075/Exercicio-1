cd5871cdff1495985e7c01f0f21637be577e8469
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-26 22:41:05 -0300

    Create Ex07.cpp

---
+++ Ex07.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {  
+   int n1, n2, m;
+   cout<< "digite suas notas";
+   cin>> n1 >> n2;
+   m = (2* n1 + 3* n2) /5;
+   cout<< "sua media é:" << m;
+    return 0;
+}
