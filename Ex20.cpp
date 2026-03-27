7dd3a24e3acee177c751f99464209d54e417c4d5
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-26 22:59:58 -0300

    Create Ex20.cpp

---
+++ Ex20.cpp
@@ -0,0 +1,10 @@
+#include 
+using namespace std;
+int main () {
+    float p,a,IMC;
+    cout<<"Digite seu peso (em kg) e sua altura (em metros) ";
+    cin>>p>>a;
+    IMC = p / (a * a);
+    cout<<"Seu IMC é:"<
+    return 0;
+}
