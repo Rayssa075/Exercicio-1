3a83edf176a5a5b3fca151fe414e180dd7b2eec1
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-26 22:47:10 -0300

    Create Ex09.cpp

---
+++ Ex09.cpp
@@ -0,0 +1,10 @@
+#include 
+using namespace std;
+int main () {
+    float a, b;
+    cout<< " Qual o seu peso em quilos? ";
+    cin>> a;
+    b= a * 1000;
+    cout<< "Em gramas o seu peso é: "<< b;
+    return 0;
+}
