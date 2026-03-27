27dfd702cebe18aa0b20c036a79565931bafda10
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-26 22:48:36 -0300

    Create Ex11.cpp

---
+++ Ex11.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    float a, b, c;
+    cout<< " Insira o valor do salario minimo atualmete e o salario do funcionario ";
+    cin>> a >> b;
+    c = b / a; 
+    cout<< "O funcionario recebe "<< c << " salarios mínimos";
+    return 0;
+}
