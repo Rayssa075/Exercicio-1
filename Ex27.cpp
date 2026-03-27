0ac8f48a1dac649d6a469b0cede3d5f3d43df6f2
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-26 23:01:32 -0300

    Create Ex27.cpp

---
+++ Ex27.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () { 
+    float l1,l2,A,P;
+    cout<<"Digite os lados do comodo (em metros)";
+    cin>>l1>>l2;
+    A=l1*l2;
+    P=18*(l1*l2);
+    cout<<"A área do comodo é " <<A;
+    cout<<"m2 e sua potˆencia é "<<P;
+    return 0;
+}
