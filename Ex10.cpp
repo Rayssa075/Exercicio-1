e8b7924eb358b9c248df2acd4e658af1e2ebf4c7
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-26 22:47:29 -0300

    Create Ex10.cpp

---
+++ Ex10.cpp
@@ -0,0 +1,10 @@
+#include  <iostream>
+using namespace std;
+int main () { 
+    float b1,b2,H,At;
+    cout<<"insira, respectivamente,o valor da base maior, base menor e altura do trapézio";
+    cin>>b1>>b2>>H;
+    At=(b1+b2)*H;
+    cout<<"A área do seu trapézio é: "<<At;
+    return 0;
+}				
