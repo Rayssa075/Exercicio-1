88c1f2b16799737ca46d74d804be670ad0f75bba
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-26 23:01:08 -0300

    Create Ex26-b.cpp

---
+++ Ex26-b.cpp
@@ -0,0 +1,11 @@
+#include  <iostream>
+using namespace std;
+int main () {
+    float p, PE;
+    cout<<"insira seu peso";
+    cin>>p;
+    PE=p*0.8;
+    cout<<"seu peso emagrecendo 20% é: "<<PE;
+    return 0;
+}
+					
