ba54a5080f8eda7c4c592609e48db159b56c4ef9
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-30 23:35:17 -0300

    Update Ex21.cpp

--- Ex21.cpp
+++ Ex21.cpp
@@ -1,16 +1,18 @@
-#include 
+#include <iostream>
 using namespace std;
 int main() {
     const float PERCENTUAL_CUSTO = 0.8;
-    float valor_do_caminhão_de_lenha, custo_de_producao, lucro;
+    float valor_do_caminhao_de_lenha, custo_de_producao, lucro;
     
     cout<<"Digite o valor do caminhão de lenha";
-    cin>> valor_do_caminhão_de_lenha;
+    cin>> valor_do_caminhao_de_lenha;
 
-    custo_de_producao = valor_do_caminhão_de_lenha * PERCENTUAL_CUSTO;
-    lucro = valor_do_caminhão_de_lenha - custo_de_producao; 
+    custo_de_producao = valor_do_caminhao_de_lenha * PERCENTUAL_CUSTO;
+    lucro = valor_do_caminhao_de_lenha - custo_de_producao; 
 
     cout<<"\n------Detalhamento do salario------\n";
     cout<<"\nO custo de produção é: " << custo_de_producao << endl;
     cout<<"O lucro é: " << lucro << endl;
+
+    return 0;
 }
