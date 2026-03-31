fea21247d292720c8bdc2a8d51426980f2fe07ff
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-30 23:34:06 -0300

    Update Ex10.cpp

--- Ex10.cpp
+++ Ex10.cpp
@@ -1,10 +1,20 @@
 #include  <iostream>
 using namespace std;
-int main () { 
-    float b1,b2,H,At;
-    cout<<"insira, respectivamente,o valor da base maior, base menor e altura do trapézio";
-    cin>>b1>>b2>>H;
-    At=(b1+b2)*H;
-    cout<<"A área do seu trapézio é: "<<At;
+int main () {
+    float base_maior;
+cout<<"Digite o valor da base maior do trpezio";
+cin>>base_maior;
+    float base_menor;
+    cout<<"Digite o valor da base menor do trapesio";
+    cin>> base_menor;
+    float altura; 
+    cout<<"Digite o valor da altura do trapesio";
+    cin>> altura;
+
+    float area;
+    if (area=  ((base_maior + base_menor) * altura) / 2) {
+        cout<<" A area do trapezio é:"<<area<< endl;
+    }
+
     return 0;
-}				
+}
