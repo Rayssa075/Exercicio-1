4d67806d11895e388fe4a4b08ab28e15104d13ee
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-30 23:34:30 -0300

    Update Ex19.cpp

--- Ex19.cpp
+++ Ex19.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 using namespace std; 
 int main() {
-    float saldo, cheque1, cheque2, taxa1, taxa2, salario;
-    cout<< "digite o valor do seu salario, do cheque 1, cheque 2 ";
-    cin>> salario >> cheque1 >> cheque2;
-    taxa1 = cheque1 * 0.0038;
-    taxa2= cheque2 * 0.0038;
-    saldo = salario + cheque1 - taxa1 + cheque2 - taxa2;
-    cout<< "O valor do seu saldo atual é: " << saldo;
+
+    const float CPMF = 0.0038;
+
+    float salario;
+    cout<<"Digite o valor do salatio depositado";
+    cin>> salario;
+
+    float saldo_atual
+
+    float cheque1;
+    cput<<"Digite o valor do primeiro cheque";
+    cin>> cheque1;
+
+    float cheque2;
+    cout<<"Digite o valor do segundo cheque";
+    cin>> cheque2;
+    
+    float taxa1= cheque1 * CPMF;
+    float taxa2= cheque2 * CPMF;
+
+    saldo_atual = salario -(cheque1 + taxa1) - (cheque2 + taxa2);
+
+    cout << fixed << setprecision(2);
+    cout << "\n--- Resumo da Conta ---" << endl;
+    cout << "Imposto sobre cheque 1: R$ " << taxa1 << endl;
+    cout << "Imposto sobre cheque 2: R$ " << taxa2 << endl;
+    cout << "Saldo atual da conta: R$ " << saldo_atual << endl;
     
    return 0;
 }
