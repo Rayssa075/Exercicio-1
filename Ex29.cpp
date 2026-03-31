2890c58970891385325dffd771cce045a69fc2ff
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-30 23:37:51 -0300

    Update Ex29.cpp

--- Ex29.cpp
+++ Ex29.cpp
@@ -1,20 +1,27 @@
-#include <iostream> 
+#include <iostream>
+
 using namespace std;
+
 int main() {
     float numero_real;
-    Float parte_inteira, parte_fracionaria, arredondada;
 
-    cout<<"Digite um numero real";
-    cin>>numero_real;
+    float parte_inteira, parte_fracionaria, arredondada;
+
+    cout << "Digite um numero real: ";
+    cin >> numero_real;
+
 
     parte_inteira = static_cast<int>(numero_real);
-    parte_fracionaria = numero_real - parte_inteira;        
-    arredondada = (parte_fracionaria >= 0.5) ? parte_inteira + 1 : parte_inteira;
 
-    cout<<"\nO numero arredondado é: "<< arredondada;
-    cout<<"\nA parte inteira é: "<< parte_inteira;
-    cout<<"\nA parte fracionaria é: "<< parte_fracionaria;
+
+    parte_fracionaria = numero_real - parte_inteira;
+
+    
+    arredondada = (parte_fracionaria >= 0.5) ? (parte_inteira + 1) : parte_inteira;
+
+    cout << "\nA parte inteira e: " << parte_inteira;
+    cout << "\nA parte fracionaria e: " << parte_fracionaria;
+    cout << "\nO numero arredondado e: " << arredondada << endl;
+
     return 0;
-
-
 }
